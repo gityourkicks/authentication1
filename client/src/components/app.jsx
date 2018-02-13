e41a2360b1bd0239116b901629273c58e0c9bf65
Benjamin Leavitt <wordsforben@gmail.com>
2018-02-13 11:45:21 -0600

    commit passport

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import HelloWorld from './hello';
 import GoodbyeWorld from './goodbye';
+import PrivateRoute from './auth/privateRoute';
+import Login from './auth/login';
+import Logout from './auth/logout';
+import AuthButton from './auth/authButton';
 
 class Navigation extends Component {
 
@@ -10,9 +14,12 @@ class Navigation extends Component {
             <Router>
                 <Fragment>
                     <Link to="/goodbye">Goodbye</Link>
+                    <AuthButton />
                     <Switch>
                         <Route exact path="/" component={HelloWorld} />
-                        <Route path="/goodbye" component={GoodbyeWorld} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/logout" component={Logout} />
+                        <PrivateRoute path="/goodbye" component={GoodbyeWorld} />
                     </Switch>
                 </Fragment>
             </Router>
